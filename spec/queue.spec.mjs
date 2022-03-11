b5fa97d59610f705c668c2d19f15c38d4a195319
steve <steve.huckle@gmail.com>
2022-03-11 16:54:44 +0000

    queue and stack

---
+++ spec/queue.spec.mjs
@@ -0,0 +1,106 @@
+import { queue } from '../src/utils/queue.mjs'
+
+describe('enqueues', function() {
+    it('correctly enqueues data', function() {
+
+        const myQueue = queue();
+        myQueue.enQueue(1);
+        myQueue.enQueue(2);
+        myQueue.enQueue(3);
+       
+        const thisQueue = myQueue.toArray();
+        //console.log('enqueues array', thisQueue);
+       
+        expect(thisQueue).toEqual([1, 2, 3]);
+    })
+})
+
+describe('dequeues', function() {
+    it('correctly dequeues data', function() {
+
+        const myQueue = queue();
+        myQueue.enQueue(1);
+        myQueue.enQueue(2);
+        myQueue.enQueue(3);
+        const myData = myQueue.deQueue();
+
+        /*const thisQueue = myQueue.toArray();
+        console.log('dequeues array', thisQueue);*/
+        
+        expect(myData).toEqual(1);
+    })
+})
+
+describe('enqueue size', function() {
+    it('correctly gets size after enqueues', function() {
+
+        const myQueue = queue();
+        const size = 100;
+
+        for( let i = 1; i <= size; i++ ) {
+            myQueue.enQueue(i);
+        }
+
+        /*const thisQueue = myQueue.toArray();
+        console.log('enqueue size array', thisQueue);*/
+
+        const mySize = myQueue.getSize();    
+        expect(mySize).toEqual(size);
+    })
+})
+
+describe('dequeue size', function() {
+    it('correctly gets size after dequeues', function() {
+
+        const myQueue = queue();
+        const size = 100;
+
+        for( let i = 1; i <= size; i++ ) {
+            myQueue.enQueue(i);
+        }
+
+        for( let i = 1; i <= size / 2; i++ ) {
+            myQueue.deQueue();
+        }
+        
+        /*const thisQueue = myQueue.toArray();
+        console.log('dequeue size array', thisQueue);*/
+
+        const mySize = myQueue.getSize();
+        expect(mySize).toEqual(size / 2);
+    })
+})
+
+describe('enqueue array', function() {
+    it('correctly gets array after enqueue', function() {
+
+        const myQueue = queue();
+        const size = 10;
+
+        for( let i = 1; i <= size; i++ ) {
+            myQueue.enQueue(i);
+        }
+
+        const thisQueue = myQueue.toArray();
+        expect(thisQueue).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    })
+})
+
+describe('dequeue array', function() {
+    it('correctly gets array after dequeue', function() {
+
+        const myQueue = queue();
+        const size = 10;
+
+        for( let i = 1; i <= size; i++ ) {
+            myQueue.enQueue(i);
+        }
+
+        for( let i = 1; i <= size / 2; i++ ) {
+            myQueue.deQueue();
+        }
+
+        const thisQueue = myQueue.toArray();
+        expect(thisQueue).toEqual([6, 7, 8, 9, 10]);
+    })
+})
---
+++ spec/stack.spec.mjs
@@ -0,0 +1,106 @@
+import { stack } from '../src/utils/stack.mjs'
+
+describe('push', function() {
+    it('correctly pushes data', function() {
+
+        const myStack = stack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+       
+        const thisStack = myStack.toArray();
+        // console.log('pushes array', thisStack);
+       
+        expect(thisStack).toEqual([3, 2, 1]);
+    })
+})
+
+describe('pop', function() {
+    it('correctly pops data', function() {
+
+        const myStack = stack();
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+        const myData = myStack.pop();
+
+        /*const thisStack = myStack.toArray();
+        console.log('pops array', thisStack);*/
+        
+        expect(myData).toEqual(3);
+    })
+})
+
+describe('push stack size', function() {
+    it('correctly gets size after pushes', function() {
+
+        const myStack = stack();
+        const size = 100;
+
+        for( let i = 1; i <= size; i++ ) {
+            myStack.push(i);
+        }
+
+        /*const thisStack = myStack.toArray();
+        console.log('push size array', thisStack);*/
+
+        const mySize = myStack.getSize();    
+        expect(mySize).toEqual(size);
+    })
+})
+
+describe('pop stack size', function() {
+    it('correctly gets size after pops', function() {
+
+        const myStack = stack();
+        const size = 100;
+
+        for( let i = 1; i <= size; i++ ) {
+            myStack.push(i);
+        }
+
+        for( let i = 1; i <= size / 2; i++ ) {
+            myStack.pop();
+        }
+        
+        /*const thisStack = myStack.toArray();
+        console.log('pops size array', thisStack);*/
+
+        const mySize = myStack.getSize();
+        expect(mySize).toEqual(size / 2);
+    })
+})
+
+describe('push array', function() {
+    it('correctly gets array after pushes', function() {
+
+        const myStack = stack();
+        const size = 10;
+
+        for( let i = 1; i <= size; i++ ) {
+            myStack.push(i);
+        }
+
+        const thisStack = myStack.toArray();
+        expect(thisStack).toEqual([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]);
+    })
+})
+
+describe('pop array', function() {
+    it('correctly gets array after pops', function() {
+
+        const myStack = stack();
+        const size = 10;
+
+        for( let i = 1; i <= size; i++ ) {
+            myStack.push(i);
+        }
+
+        for( let i = 1; i <= size / 2; i++ ) {
+            myStack.pop();
+        }
+
+        const thisStack = myStack.toArray();
+        expect(thisStack).toEqual([5, 4, 3, 2, 1]);
+    })
+})
